0da30ad9adeb092f9da4b4a488983927a93b6977
Manoj Kumar <manojkr55@gmail.com>
2022-12-04 18:56:04 +0530

    Image Slider added

---
+++ image-slider/js/app.js
@@ -0,0 +1,35 @@
+const slides = document.querySelectorAll(".slide");
+var counter = 0;
+
+slides.forEach(
+    (slide, index) => {
+        slide.style.left = `${index * 100}%`
+    }
+)
+
+const goPrev = () => {
+    if (counter == 0) {
+        counter = slides.length-1
+    } else {
+        counter--
+    }
+    slideImage()
+}
+
+const goNext = () => {
+    if (counter==slides.length-1) {
+        counter = 0
+    } else {
+        counter++
+    }
+    
+    slideImage()
+}
+
+const slideImage = () => {
+    slides.forEach(
+        (slide) => {
+            slide.style.transform = `translateX(-${counter * 100}%)`
+        }
+    )
+}
